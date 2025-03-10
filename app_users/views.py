b318b071d9a5bd1bd95234270d64948bbcef4bb3
gold-code-dev <gold.code.dev@gmail.com>
2025-03-09 23:20:43 -0300

    ti ti@mail.com 123

--- app_users/views.py
+++ app_users/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+# View para exibir e processar login
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('painel')  # Após login, redireciona para o painel
+        else:
+            return render(
+                request,
+                'app_users/login.html',
+                {'error': 'Credenciais inválidas'}
+            )
+
+    return render(request, 'app_users/login.html')
+
+
+# View para o painel (login obrigatório)
+@login_required  # Certifica que somente usuários autenticados podem acessar
+def painel_view(request):
+    return render(
+        request,
+        'app_users/painel.html',
+        {
+            'username': request.user.username,  # Nome do usuário
+            'email': request.user.email,  # Email do usuário
+        }
+    )
