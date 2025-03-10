f4526699b296d4bf32a37f65c52087b201165a67
gold-code-dev <gold.code.dev@gmail.com>
2025-03-10 16:52:06 -0300

    sucess2

--- app_users/views.py
+++ app_users/views.py
@@ -75,6 +75,7 @@ from .models import Escritorio, RelacaoColaborador
     # return render(request, "painel.html", context)
 #
 
+"""
 @login_required
 def painel_view(request):
     context = {}
@@ -93,4 +94,32 @@ def painel_view(request):
         context['usuarios'] = [colaboracao.colaborador]
 
     # Renderiza o painel com o contexto preenchido
+    return render(request, "app_users/painel.html", context)
+"""
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Escritorio, RelacaoColaborador
+
+@login_required
+def painel_view(request):
+    context = {}
+
+    if hasattr(request.user, 'escritorio'):  # É contador
+        escritorio = request.user.escritorio
+        colaboradores = RelacaoColaborador.objects.filter(escritorio=escritorio)
+        context['tipo_usuario'] = 'contador'
+        context['usuarios'] = [escritorio.contador] + [colaborador.colaborador for colaborador in colaboradores]
+
+    elif hasattr(request.user, 'colaboracao'):  # É colaborador
+        colaboracao = request.user.colaboracao
+        escritorio = colaboracao.escritorio
+        colaboradores = RelacaoColaborador.objects.filter(escritorio=escritorio)
+        context['tipo_usuario'] = 'colaborador'
+        context['usuarios'] = [escritorio.contador] + [colaborador.colaborador for colaborador in colaboradores]
+
+    else:  # Caso o usuário não tenha os atributos necessários
+        context['tipo_usuario'] = 'desconhecido'
+        context['usuarios'] = []  # Nenhum usuário relacionado
+
     return render(request, "app_users/painel.html", context)
