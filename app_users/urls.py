0138fccabc86b65ccad8572d36533372d1ff9ba0
gold-code-dev <gold.code.dev@gmail.com>
2025-03-11 08:27:44 -0300

    update

--- app_users/urls.py
+++ app_users/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from django.shortcuts import redirect
+
 
 urlpatterns = [
     path('login/', views.login_view, name='login'),  # Página de login
     path('painel/', views.painel_view, name='painel'),  # Painel (após login)
     path('logout/', views.logout_view, name='logout'),  # Logout
+    path("", lambda request: redirect("login")),
+
 ]
