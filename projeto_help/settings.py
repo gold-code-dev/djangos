93bf6a13431441e9cbd9fdd15d46a47221454de1
gold-code-dev <gold.code.dev@gmail.com>
2025-03-11 20:20:50 -0300

    ajustes2

--- projeto_help/settings.py
+++ projeto_help/settings.py
@@ -39,9 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_users',
-    # 'app_escritorios',
-
+    'app_core',
 ]
 
 MIDDLEWARE = [
--- projeto_help/urls.py
+++ projeto_help/urls.py
@@ -4,12 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    # path('admin/', admin.site.urls), # nao vou usar admin
-    path('', include('app_users.urls')),  # Incluímos as rotas do app_users
-    # path('escritorios/', include('app_escritorios.urls')),  # Rotas do app_escritorios
-
+    path('', include('app_core.urls')),  # Incluímos as rotas do app_core
 ]
 
 # Servir arquivos de mídia (como logomarcas) em DEBUG
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
