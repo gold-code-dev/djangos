8c4914d01336b014ba1f5bc685bcc742b355290e
gold-code-dev <gold.code.dev@gmail.com>
2025-03-11 14:07:47 -0300

    update

--- app_users/views/base.py
+++ app_users/views/base.py
@@ -7,9 +7,15 @@ def base_view(request):
     usuario = request.user
     nome_completo = usuario.get_full_name()
 
-    # Se o nome completo estiver em branco (o que costuma ser o padrão), use o username.
+    # Se o nome completo estiver em branco (o que costuma ser o padrão), use o username
     if not nome_completo:
         nome_completo = usuario.username
 
-    context = {'nome_completo': nome_completo}
+    # Obter o primeiro nome do usuário
+    first_name = usuario.first_name or usuario.username  # Usa o primeiro nome; caso esteja em branco, usa o username
+
+    context = {
+        'nome_completo': nome_completo,
+        'first_name': first_name,  # Incluímos o primeiro nome no contexto
+    }
     return render(request, 'app_users/base.html', context)
