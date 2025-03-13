4e65a49a184e3bd47b95ab6f1f5a818699fddefe
gold-code-dev <gold.code.dev@gmail.com>
2025-03-13 15:12:19 -0300

    update

--- app_core/views/painel_view.py
+++ app_core/views/painel_view.py
@@ -6,16 +6,16 @@ from django.contrib.auth.models import User
 @login_required
 def painel_view(request):
     usuario = request.user
-    nome_completo = usuario.get_full_name() or usuario.username  # Nome completo ou username
+    # nome_completo = usuario.get_full_name() or usuario.username  # Nome completo ou username
     first_name = usuario.first_name or usuario.username  # Primeiro nome ou username
 
     # Obter todos os usuários do escritório (exemplo, ajuste para seu modelo)
-    usuarios = User.objects.all()
+    # usuarios = User.objects.all()
 
     context = {
-        'nome_completo': nome_completo,
+        # 'nome_completo': nome_completo,
         'first_name': first_name,
-        'escritorio': "Escritório Modelo",  # Substitua por uma variável ou busca no banco de dados
-        'usuarios': usuarios,  # Incluímos a lista de usuários
+        # 'escritorio': "Escritório Modelo",  # Substitua por uma variável ou busca no banco de dados
+        # 'usuarios': usuarios,  # Incluímos a lista de usuários
     }
     return render(request, 'app_core/painel.html', context)
