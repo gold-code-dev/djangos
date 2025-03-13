055ce47c31b0b25ff3935d370cd08e5af844f3eb
gold-code-dev <gold.code.dev@gmail.com>
2025-03-13 12:37:54 -0300

    update

--- app_core/views/login_view.py
+++ app_core/views/login_view.py
@@ -1,20 +1,32 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
 from django.http import HttpResponseNotAllowed
 
 
 # View para exibir e processar login
 def login_view(request):
+    if request.user.is_authenticated:
+        # Redireciona para 'painel' se o usuário já estiver logado
+        return redirect('painel')
+
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if not username or not password:
+            # Verificação de campos ausentes
+            return render(
+                request,
+                'app_core/login.html',
+                {'error': 'Os campos de usuário e senha são obrigatórios!'}
+            )
+
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
-            return redirect('base')  # Após login, redireciona para o painel
+            # Após login, redirecionar para a rota 'painel'
+            return redirect('painel')
         else:
             return render(
                 request,
@@ -22,4 +34,8 @@ def login_view(request):
                 {'error': 'Senha ou email incorretos!'}
             )
 
-    return render(request, 'app_core/login.html')
+    elif request.method == 'GET':
+        return render(request, 'app_core/login.html')
+
+    # Resposta para métodos não permitidos
+    return HttpResponseNotAllowed(['GET', 'POST'])
