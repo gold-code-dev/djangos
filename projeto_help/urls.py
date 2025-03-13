ae7b5193c762a8af25fddd6140544d2e16326eef
gold-code-dev <gold.code.dev@gmail.com>
2025-03-13 09:41:47 -0300

    update

--- projeto_help/urls.py
+++ projeto_help/urls.py
@@ -1,12 +1,5 @@
-from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('app_core.urls')),  # Incluímos as rotas do app_core
+    path('', include('app_core.urls')),
 ]
-
-# Servir arquivos de mídia (como logomarcas) em DEBUG
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
