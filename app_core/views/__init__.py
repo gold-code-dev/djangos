ab157d588447bcda4c788b74e70fed497c078071
gold-code-dev <gold.code.dev@gmail.com>
2025-03-13 22:08:57 -0300

    update

--- app_core/views/__init__.py
+++ app_core/views/__init__.py
@@ -2,21 +2,22 @@
 from .login_view import login_view
 from .painel_view import painel_view
 from .logout_view import logout_view
-from .ticket_new_view import ticket_new_view
-
+# from .ticket_new_view import ticket_new_view
+from .criar_ticket_view import criar_ticket
 
 
 # from .painel import painel_view
-from .ticket import criar_ticket_view
-from .ticket_list import ticket_list
+# from .ticket import criar_ticket_view
+# from .ticket_list import ticket_list
 
 __all__ = [
     "login_view",
     "painel_view",
     "logout_view",
-    "ticket_new_view",
+    "criar_ticket"
+    # "ticket_new_view",
 
 
-    "criar_ticket_view",
-    "ticket_list"
+    # "criar_ticket_view",
+    # "ticket_list"
 ]
