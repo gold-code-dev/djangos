a523495c267affabb64effefa50d6a6939351ba4
gold-code-dev <gold.code.dev@gmail.com>
2025-03-13 21:49:36 -0300

    update

--- app_core/models/ticket.py
+++ app_core/models/ticket.py
@@ -3,17 +3,19 @@ from django.contrib.auth.models import User
 
 
 class Ticket(models.Model):
-    STATUS_CHOICES = [
-        ('recebido', 'Recebido'),
-        ('enviado', 'Enviado'),
-        ('elaboracao', 'Em Elaboração'),
+    TIPO_CHOICES = [
+        ('abertura', 'Abertura'),
+        ('alteracao', 'Alteração'),
+        ('transformacao', 'Transformação'),
+        ('baixa', 'Baixa'),
     ]
 
-    titulo = models.CharField(max_length=255)
-    descricao = models.TextField()
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='elaboracao')
-    criado_em = models.DateTimeField(auto_now_add=True)
-    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True)
+    tipo = models.CharField(max_length=20, choices=TIPO_CHOICES)
+    nome_empresa = models.CharField(max_length=255)
+    prazo = models.DateTimeField()
+    criado_por = models.ForeignKey(User, on_delete=models.CASCADE)  # Relaciona ao usuário logado
+    criado_em = models.DateTimeField(auto_now_add=True)  # Preenchido automaticamente com a data/hora da criação
 
     def __str__(self):
-        return self.titulo
+        return f"Ticket {self.id} - {self.nome_empresa}"
