fdc5bc4ce1d0c63119ccbf2eea37f441d1f6da70
gold-code-dev <gold.code.dev@gmail.com>
2025-03-13 22:43:01 -0300

    update

--- app_core/forms/ticket_form.py
+++ app_core/forms/ticket_form.py
@@ -1,11 +1,15 @@
 from django import forms
 from ..models import Ticket
+from django.utils.timezone import now
 
 
 class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
-        fields = ['tipo', 'nome_empresa', 'prazo']  # Campos que o usuário pode preencher
+        fields = ['tipo', 'nome_empresa', 'prazo']
         widgets = {
-            'prazo': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'prazo': forms.DateTimeInput(attrs={
+                'type': 'datetime-local',
+                'min': now().strftime('%Y-%m-%dT%H:%M')  # Data e hora mínimas para hoje
+            }),
         }
