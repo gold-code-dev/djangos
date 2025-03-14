4a6c30fd3a3642458ad1cc1b858114dfd8c73ee3
gold-code-dev <gold.code.dev@gmail.com>
2025-03-13 22:36:08 -0300

    update

--- app_core/views/criar_ticket_view.py
+++ app_core/views/criar_ticket_view.py
@@ -2,17 +2,23 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from ..models import Ticket
 from ..forms import TicketForm
+from django.http import HttpResponseForbidden
 
 
-@login_required  # Garante que apenas usuários logados acessem
+@login_required
 def criar_ticket(request):
+    # Garante que só vem do painel (ou você pode validar outras condições aqui)
+    if request.META.get('HTTP_REFERER') is None or 'painel/' not in request.META.get('HTTP_REFERER'):
+        msg = "Acesso não permitido digitando o path da URL.<br>Precisa seguir o fluxo do seu atendimento: clicando nos botões correspondentes."
+        return HttpResponseForbidden(msg)  # Retorna erro 403 se o acesso for direto
+
     if request.method == "POST":
         form = TicketForm(request.POST)
         if form.is_valid():
-            ticket = form.save(commit=False)  # Não salva ainda, para adicionar o "criado_por"
-            ticket.criado_por = request.user  # Adiciona o usuário logado
+            ticket = form.save(commit=False)
+            ticket.criado_por = request.user
             ticket.save()
-            return redirect("lista_tickets")  # Redireciona para a página de lista (crie uma URL para isso)
+            return redirect("lista_tickets")
     else:
         form = TicketForm()
     return render(request, "app_core/criar_ticket.html", {"form": form})
