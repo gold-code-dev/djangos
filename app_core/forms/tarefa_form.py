6c0e312173fffda3488ff56baea79e72d5a44c95
gold-code-dev <gold.code.dev@gmail.com>
2025-03-14 18:03:18 -0300

    update

---
+++ app_core/forms/tarefa_form.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import Tarefa
+
+
+class TarefaForm(forms.ModelForm):
+    class Meta:
+        model = Tarefa
+        fields = ['nome', 'descricao', 'responsavel', 'prazo', 'concluido']
+        widgets = {
+            'prazo': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Estilizar os campos ou alterar as etiquetas, se necessário
+        self.fields['responsavel'].required = True  # Torna um campo necessário
--- app_core/views/criar_tarefa_view.py
+++ app_core/views/criar_tarefa_view.py
@@ -1,24 +1,22 @@
 from django.shortcuts import render, redirect
-from ..models import Tarefa
+from django.contrib.auth.decorators import login_required  # Usuário deve estar logado para criar uma tarefa
+from .models import Tarefa
+from .forms import TarefaForm
 
 
+@login_required
 def criar_tarefa(request):
-    if request.method == 'POST':  # Verifica se os dados estão sendo enviados (via formulário)
-        nome = request.POST.get('nome')  # Pega o valor do campo "nome"
-        descricao = request.POST.get('descricao')  # Pega o valor do campo "descricao"
-        responsavel = request.POST.get('responsavel')  # Pega o valor do campo "responsável"
-        prazo = request.POST.get('prazo')  # Pega o prazo (se existir)
+    if request.method == 'POST':
+        # Instancia o formulário com os dados submetidos via POST
+        form = TarefaForm(request.POST)
+        if form.is_valid():
+            tarefa = form.save(commit=False)  # Cria o objeto tarefa sem salvar ainda
+            tarefa.criado_por = request.user  # Define o usuário atual como criador
+            tarefa.save()  # Salva a tarefa no banco de dados
+            return redirect('/tarefas/')  # Redireciona para a listagem ou outra página
+    else:
+        # Exibe o formulário vazio para criação
+        form = TarefaForm()
 
-        # Criação da nova tarefa no banco de dados
-        Tarefa.objects.create(
-            nome=nome,
-            descricao=descricao,
-            responsavel=responsavel,
-            prazo=prazo
-        )
-
-        # Após criar, redireciona o usuário para a página de listagem (ou outra desejada)
-        return redirect('/tarefas/')
-
-    # Caso contrário (GET), exibe o formulário
-    return render(request, 'tarefas/criar_tarefa.html')
+    # Renderiza o template com o formulário
+    return render(request, 'app_core/criar_tarefa.html', {'form': form})
