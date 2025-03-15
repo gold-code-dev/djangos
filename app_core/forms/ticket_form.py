74d3f2cfd39be54828b2551eca480b3ff4d8f17b
gold-code-dev <gold.code.dev@gmail.com>
2025-03-14 22:05:41 -0300

    update

--- app_core/forms/ticket_form.py
+++ app_core/forms/ticket_form.py
@@ -3,16 +3,13 @@ from ..models import Ticket
 from django.utils.timezone import now
 
 
-from ..models import Tarefa, Ticket
-
-
-class TarefaForm(forms.ModelForm):
+class TicketForm(forms.ModelForm):
     class Meta:
-        model = Tarefa
-        fields = ['nome', 'descricao', 'responsavel', 'prazo', 'concluido', 'ticket']  # Adicione o campo
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # Tornar o ticket invisível no formulário (caso necessário, por exemplo, se for definido pela view)
-        self.fields['ticket'].widget = forms.HiddenInput()
-        self.fields['responsavel'].required = True
+        model = Ticket
+        fields = ['tipo', 'nome_empresa', 'prazo']
+        widgets = {
+            'prazo': forms.DateTimeInput(attrs={
+                'type': 'datetime-local',
+                'min': now().strftime('%Y-%m-%dT%H:%M')  # Data e hora mínimas para hoje
+            }),
+        }
