2f8df37371aab340a975cbeebf986b281b6c2b93
gold-code-dev <gold.code.dev@gmail.com>
2025-03-14 21:43:25 -0300

    update

--- app_core/views/ticket_list.py
+++
@@ -1,5 +0,0 @@
-# from django.shortcuts import render
-#
-#
-# def ticket_list(request):
-#     return render(request, 'ticket_list.html')
--- app_core/views/ticket_new_view.py
+++
@@ -1,8 +0,0 @@
-# from django.shortcuts import render, redirect
-# from django.contrib.auth.decorators import login_required  # Para exigir autenticação
-#
-#
-# @login_required(login_url='/login/')  # Redireciona para '/login/' se não autenticado
-# def ticket_new_view(request):
-#     # Apenas renderiza a página HTML (sem passar dados ou tratar POST ainda)
-#     return render(request, "app_core/ticket_new.html")
