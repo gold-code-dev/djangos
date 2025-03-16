926e9499fa0bfb79a2dc480abd67b55792f07fb4
gold-code-dev <gold.code.dev@gmail.com>
2025-03-15 21:08:15 -0300

    update

--- app_core/models/atendimento.py
+++ app_core/models/atendimento.py
@@ -10,19 +10,18 @@ class Atendimento(models.Model):
     criado_em = models.DateTimeField(auto_now_add=True)  # Preenchido automaticamente com a data/hora da criação
     escritorio = models.ForeignKey(Escritorio, on_delete=models.CASCADE,
                                    related_name='atendimentos')  # Relaciona ao Escritório
-    numero_escritorio = models.PositiveIntegerField()  # Numeração do atendimento dentro do escritório
+    id_escritorio = models.PositiveIntegerField()  # ID sequencial do atendimento dentro do escritório
 
     def save(self, *args, **kwargs):
-        """Adiciona a lógica para o número único por escritório."""
+        """Adiciona a lógica para o ID único por escritório."""
         if not self.pk:  # Apenas para novos objetos
-            ultimo_atendimento = Atendimento.objects.filter(escritorio=self.escritorio).order_by(
-                'numero_escritorio').last()
+            ultimo_atendimento = Atendimento.objects.filter(escritorio=self.escritorio).order_by('id_escritorio').last()
             if ultimo_atendimento:
-                self.numero_escritorio = ultimo_atendimento.numero_escritorio + 1
+                self.id_escritorio = ultimo_atendimento.id_escritorio + 1
             else:
-                self.numero_escritorio = 1  # Primeiro atendimento do escritório
+                self.id_escritorio = 1  # Primeiro atendimento do escritório
 
         super().save(*args, **kwargs)  # Salva o atendimento no banco de dados
 
     def __str__(self):
-        return f"Atendimento {self.numero_escritorio} - {self.nome_empresa}"
+        return f"Atendimento {self.id_escritorio} - {self.nome_empresa}"
